ddc03bd5d17ac493452aacea4ed29385f193c5f2
ICF2025832 <icf2025832@ICF2025832.local>
2018-09-27 14:42:56 +0530

    Basic Hapi Server

--- basicHapiServer.js
+++ basicHapiServer.js
@@ -4,3 +4,29 @@
  * Importing Hapi JS
  */
 const Hapi = require('hapi');
+
+/**
+ * Initializing the server object
+ */
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+/**
+ * Method to initialize the server
+ */
+const init = async () => {
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);    
+};
+
+/**
+ * Quiting the server if any unhandled rejections were encountered
+ */
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
